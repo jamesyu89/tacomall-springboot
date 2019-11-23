f39a2e49700e0e6650db8610e66ba153b1f61fa8
kyeteo <3189482282@qq.com>
2019-11-23 11:11:22 +0800

    1，添加注释模板

--- tacomall-springboot-api/tacomall-springboot-api-portal/src/main/java/cn/tacomall/tacomallspringbootapiportal/TacomallSpringbootApiPortal.java
+++ tacomall-springboot-api/tacomall-springboot-api-portal/src/main/java/cn/tacomall/tacomallspringbootapiportal/TacomallSpringbootApiPortal.java
@@ -1,9 +1,26 @@
+/**
+ * projectName: tacomall-sprinboot
+ * fileName: TacomallSpringbootApiPortal.java
+ * packageName: cn.tacomall.tacomallsprinbootapiportal
+ * date: 2019年11月23日下午12:28:39
+ * copyright(c) 2019-2020 芒果教育科技有限公司
+ */
+
 package cn.tacomall.tacomallspringbootapiportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @version: V1.0
+ * @author: running-cat
+ * @className: TacomallSpringbootApiPortal
+ * @packageName: cn.tacomall.tacomallsprinbootapiportal
+ * @description: 移动端启动类
+ * @data: 2019年11月23日下午12:28:39
+ **/
 
 @SpringBootApplication(scanBasePackages="cn.tacomall")
 @MapperScan("cn.tacomall.tacomallspringbootmapper")
--- tacomall-springboot-api/tacomall-springboot-api-portal/src/main/java/cn/tacomall/tacomallspringbootapiportal/controller/Index.java
+++ tacomall-springboot-api/tacomall-springboot-api-portal/src/main/java/cn/tacomall/tacomallspringbootapiportal/controller/Index.java
@@ -1,3 +1,16 @@
+/**
+ * projectName: tacomall-sprinboot
+ * fileName: Index.java
+ * packageName: cn.tacomall.tacomallsprinbootapiportal.controller
+ * date: 2019年11月23日下午12:28:39
+ *
+ * 修改履历:
+ *     日期                          修正者           主要内容
+ *     2019年11月23日下午12:28:39    running-cat      初版完成
+ *
+ * copyright(c) 2019-2020 芒果教育科技有限公司
+ */
+
 package cn.tacomall.tacomallspringbootapiportal.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +24,28 @@ import cn.tacomall.tacomallspringbootutils.RequestUtil;
 import cn.tacomall.tacomallspringbootutils.ResponseUtil;
 import cn.tacomall.tacomallspringbootapiportal.annotation.IgnoreAuth;
 
+/**
+ * @author: running-cat
+ * @className: Index
+ * @packageName: cn.tacomall.tacomallsprinbootapiportal.controller
+ * @description: 移动端默认入口
+ * @data: 2019年11月23日下午12:28:39
+ **/
+
 @Api(tags = "portal domain and index module")
 @RestController
 @RequestMapping(value = "/portal/index/")
 public class Index {
+
+    /**
+     * @author:  running-cat
+     * @methodsName: index
+     * @description: 默认方法
+     * @param:  code 测试验证码
+     * @return: ResponseDto
+     * @throws:
+     */
+
     @ApiOperation(value = "index", notes = "index method", httpMethod = "POST")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "code", value = "verify code", required = true, paramType = "path"),
@@ -22,7 +53,11 @@ public class Index {
     @IgnoreAuth
     @PostMapping("index")
     public ResponseDto index(@RequestBody RequestUtil jsonRequest, ResponseUtil responseUtil) throws Exception {
+        /**
+         * 使用jsonRequest.getStr获取客户端传来的code
+         * 无则抛出ClientException
+         */
         String code = jsonRequest.getStr("code");
-        return responseUtil.success(code);
+        return responseUtil.success(code); // 返回ResponseDto对象
     }
 }
