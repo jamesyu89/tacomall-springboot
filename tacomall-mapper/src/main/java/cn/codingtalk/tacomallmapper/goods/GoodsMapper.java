194272aa681114bbb17baa79c0b0a4cf8ee8d98e
jerry tang <jerry.tang.haibiao@gmail.com>
2020-06-11 21:35:44 +0800

    :poop: (test!!!)vscode配置

--- tacomall-mapper/src/main/java/cn/codingtalk/tacomallmapper/goods/GoodsMapper.java
+++ tacomall-mapper/src/main/java/cn/codingtalk/tacomallmapper/goods/GoodsMapper.java
@@ -1,3 +1,12 @@
+/***
+ * @Author: 码上talk|RC
+ * @Date: 2020-06-09 23:20:41
+ * @LastEditTime: 2020-06-11 21:30:57
+ * @LastEditors: 码上talk|RC
+ * @Description: 
+ * @FilePath: \tacomall-springboot\tacomall-mapper\src\main\java\cn\codingtalk\tacomallmapper\goods\GoodsMapper.java
+ * @Just do what I think it is right
+ */
 package cn.codingtalk.tacomallmapper.goods;
 
 import org.apache.ibatis.annotations.Param;
